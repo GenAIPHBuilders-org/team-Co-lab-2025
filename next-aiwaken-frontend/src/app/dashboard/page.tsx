109b92b83c1c30ba03a5da73c5267759a188360a
B J <brianjamesdlcz@gmail.com>
2025-05-07 21:00:29 +0800

    feat(topic-selection): added user topic selection ui at the dashboard and a dialog for confirmation

--- next-aiwaken-frontend/src/app/dashboard/page.tsx
+++ next-aiwaken-frontend/src/app/dashboard/page.tsx
@@ -1,12 +1,9 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
-import {
-  SidebarInset,
-  SidebarProvider,
-} from "@/components/ui/sidebar"
+import { TopicSelectionCard } from "@/components/topic-selection"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
 export const iframeHeight = "800px"
-
 export const description = "A sidebar with a header and a search form."
 
 export default function Page() {
@@ -19,7 +16,7 @@ export default function Page() {
           <SidebarInset>
             <div className="flex flex-1 flex-col gap-4 p-4">
               <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <TopicSelectionCard />
                 <div className="bg-muted/50 aspect-video rounded-xl" />
                 <div className="bg-muted/50 aspect-video rounded-xl" />
               </div>
